ff59c7e04f6dd16789a61bf7f87464df598ba952
fares-soliman <f.soliman123@gmail.com>
2019-10-01 00:18:21 -0400

    added functionality via Intent

--- app/src/main/java/com/example/transnooze/EnterNameActivity.java
+++ app/src/main/java/com/example/transnooze/EnterNameActivity.java
@@ -1,8 +1,11 @@
 package com.example.transnooze;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class EnterNameActivity extends AppCompatActivity {
 
@@ -12,7 +15,23 @@ public class EnterNameActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         setContentView(R.layout.activity_enter_name);
 
+        final String nameOfPlace = (String) getIntent().getExtras().get("nameOfPlace");
+
         Button button = findViewById(R.id.button);
+        final EditText nameInputText = findViewById(R.id.nameInputText);
+
+        //transfers given names back to MainActivity to be added into ListView
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent nameToList = new Intent(getBaseContext(), MainActivity.class);
+                nameToList.putExtra("nameOfPlace", nameOfPlace);
+                nameToList.putExtra("mainName", nameInputText.getText().toString());
+                startActivity(nameToList);
+            }
+        });
+
+
     }
 
 
